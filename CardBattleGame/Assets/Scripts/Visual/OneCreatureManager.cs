4b91712ff1a88eeb17426b3a85ec5130eb6bf64d
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2020-06-20 15:28:56 +0100

    Partial fix for prefabs
    Propably prefabs will require more adjusting in the future

---
+++ CardBattleGame/Assets/Scripts/Visual/OneCreatureManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OneCreatureManager : MonoBehaviour
+{
+    public CardAsset cardAsset;
+    public OneCardManager PreviewManager;
+    [Header("Text Component References")]
+    public Text HealthText;
+    public Text AttackText;
+    [Header("Image References")]
+    public Image CreatureGraphicImage;
+    public Image CreatureGlowImage;
+
+    private void Awake()
+    {
+        if (cardAsset != null)
+            ReadCreatureFromAsset();
+    }
+
+    private bool canAttackNow = false;
+    public bool CanAttackNow
+    {
+        get
+        {
+            return canAttackNow;
+        }
+        set
+        {
+            canAttackNow = value;
+            CreatureGlowImage.enabled = value;
+        }
+    }
+
+
+
+    private void ReadCreatureFromAsset()
+    {
+        // Change the card graphic sprite
+        CreatureGraphicImage.sprite = cardAsset.CardImage;
+
+        AttackText.text = cardAsset.Attack.ToString();
+        HealthText.text = cardAsset.MaxHealth.ToString();
+
+        if(PreviewManager != null)
+        {
+            PreviewManager.cardAsset = cardAsset;
+            PreviewManager.ReadCardFromAsset();
+        }
+    }
+
+    public void TakeDamage(int amount, int healthAfter)
+    {
+        if(amount > 0)
+        {
+            // TODO DamageEffect.CreateDamageEffect(transform.position, amount);
+            HealthText.text = healthAfter.ToString();
+        }
+    }
+}
